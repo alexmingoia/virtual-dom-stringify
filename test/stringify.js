b8fb8c75bf9a30aab940259fe3a6308657f1e6da
Alex Mingoia <talk@alexmingoia.com>
2015-01-06 10:43:37 -0800

    Switch vtree dependency to virtual-dom

--- test/stringify.js
+++ test/stringify.js
@@ -1,12 +1,13 @@
 var expect = require('expect.js');
-var VirtualNode = require('vtree/vnode');
-var VirtualText = require('vtree/vtext');
-var svg = require('virtual-hyperscript/svg');
+var h = require('virtual-dom/h');
+var VirtualNode = require('virtual-dom/vnode/vnode');
+var VirtualText = require('virtual-dom/vnode/vtext');
+var svg = require('virtual-dom/virtual-hyperscript/svg');
 var stringify = require('..');
 
 describe('stringify()', function() {
   it('returns string', function() {
-    var vnode = new VirtualNode('div');
+    var vnode = h('div');
     var html = stringify(vnode);
     expect(html).to.be.a('string');
     expect(html).to.equal('<div></div>');
